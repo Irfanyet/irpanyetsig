1fa5aa09342bce6ff0e58640ed687834e7e722a8
Irfanyet <108773724+Irfanyet@users.noreply.github.com>
2022-07-06 08:26:30 +0700

    Add files via upload

---
+++ PETA1/data/DBD_1.js
@@ -0,0 +1,24 @@
+var json_DBD_1 = {
+"type": "FeatureCollection",
+"name": "DBD_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "JML KASUS": "11" }, "geometry": { "type": "Point", "coordinates": [ 111.284918275186016, -6.809134863697045 ] } },
+{ "type": "Feature", "properties": { "JML KASUS": "43" }, "geometry": { "type": "Point", "coordinates": [ 111.28042392836015, -6.731950942528055 ] } },
+{ "type": "Feature", "properties": { "JML KASUS": "104" }, "geometry": { "type": "Point", "coordinates": [ 111.372966748203893, -6.731950942528055 ] } },
+{ "type": "Feature", "properties": { "JML KASUS": "32" }, "geometry": { "type": "Point", "coordinates": [ 111.469241133363909, -6.695381602428513 ] } },
+{ "type": "Feature", "properties": { "JML KASUS": "6" }, "geometry": { "type": "Point", "coordinates": [ 111.514766230222534, -6.661051201518738 ] } },
+{ "type": "Feature", "properties": { "JML KASUS": "23" }, "geometry": { "type": "Point", "coordinates": [ 111.549842944195561, -6.690157410985721 ] } },
+{ "type": "Feature", "properties": { "JML KASUS": "5" }, "geometry": { "type": "Point", "coordinates": [ 111.595368041054172, -6.709561550630376 ] } },
+{ "type": "Feature", "properties": { "JML KASUS": "14" }, "geometry": { "type": "Point", "coordinates": [ 111.632683694216965, -6.782700230829461 ] } },
+{ "type": "Feature", "properties": { "JML KASUS": "25" }, "geometry": { "type": "Point", "coordinates": [ 111.584919658168587, -6.749862456046198 ] } },
+{ "type": "Feature", "properties": { "JML KASUS": "25" }, "geometry": { "type": "Point", "coordinates": [ 111.541633500499742, -6.767027656501086 ] } },
+{ "type": "Feature", "properties": { "JML KASUS": "25" }, "geometry": { "type": "Point", "coordinates": [ 111.578949153662535, -6.793894926778301 ] } },
+{ "type": "Feature", "properties": { "JML KASUS": "6" }, "geometry": { "type": "Point", "coordinates": [ 111.564022892397418, -6.864794667787618 ] } },
+{ "type": "Feature", "properties": { "JML KASUS": "6" }, "geometry": { "type": "Point", "coordinates": [ 111.477450577059727, -6.852107345712266 ] } },
+{ "type": "Feature", "properties": { "JML KASUS": "22" }, "geometry": { "type": "Point", "coordinates": [ 111.46327062885787, -6.781207604702949 ] } },
+{ "type": "Feature", "properties": { "JML KASUS": "13" }, "geometry": { "type": "Point", "coordinates": [ 111.478943203186248, -6.735682507844335 ] } },
+{ "type": "Feature", "properties": { "JML KASUS": "28" }, "geometry": { "type": "Point", "coordinates": [ 111.367742556761101, -6.796880179031325 ] } },
+{ "type": "Feature", "properties": { "JML KASUS": "20" }, "geometry": { "type": "Point", "coordinates": [ 111.37669831352018, -6.869272546167154 ] } }
+]
+}
